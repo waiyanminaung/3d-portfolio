20c45dd907fe47aedb484a0bba9c96d3b467569c
wyma-dss <waiyanminaung@dss.com.mm>
2025-10-02 02:40:36 +0700

    Update font imports and enhance HomeLayout structure for improved design and readability

--- src/components/3d/scence/HomeScence/Lighting.tsx
+++ src/components/3d/scence/HomeScence/Lighting.tsx
@@ -1,7 +1,7 @@
 export default function DetailLighting() {
   return (
     <>
-      <directionalLight position={[-3, -1, 1]} intensity={6} color="#FFDAB9" />
+      <directionalLight position={[-2, 2, 4]} intensity={5} color="#FFDAB9" />
       <hemisphereLight intensity={0.3} />
       <ambientLight intensity={1} />
     </>
--- src/components/layouts/HomeLayout/index.tsx
+++ src/components/layouts/HomeLayout/index.tsx
@@ -3,23 +3,43 @@ import { HomeScence } from "@/components/3d/scence";
 export default function HomeLayout() {
   return (
     <>
-      <HomeScence />
+      <div className="h-screen bg-gradient-to-r from-[#282727]  to-[#0C0C0E]">
+        <div className="pointer-events-none absolute inset-0 [background-size:200px_200px] select-none [background-image:linear-gradient(to_right,#2C2C2C_1px,transparent_1px)]" />
+        <HomeScence />
+      </div>
 
-      <div className="fixed top-2/4 w-full container left-0 right-0 mx-auto -translate-y-2/4">
-        <div className="grid grid-cols-[40%_1fr_30%] items-center">
+      <div className="fixed top-2/4 w-full container left-0 right-0 mx-auto -translate-y-2/4 text-white">
+        <div className="grid grid-cols-[40%_1fr_30%] items-end">
           <div className="text-left">
-            <h1 className="text-8xl font-semibold font-smart-heading">
-              Wai Yan
+            <h1 className="text-6xl font-semibold uppercase mb-5">
+              Frontend
               <br />
-              Min Aung
+              Developer
             </h1>
-          </div>
-          <div className="h-0" />
-          <div className="text-center">
-            <h1 className="font-code text-start text-2xl font-light">
+            <p className="text-lg max-w-[380px] font-light">
               I'm experienced in Web Development and have a passion for crafting
               digital experiences.
-            </h1>
+            </p>
+          </div>
+          <div className="h-0" />
+          <div className="flex">
+            <div className="text-start ms-auto">
+              <div className="flex gap-2 items-center">
+                <span className="inline-block size-3 bg-[#18FF97] rounded-full" />
+                <span className="text-sm font-light">Available</span>
+              </div>
+              <div className="mt-2">
+                <a
+                  href="mailto:info.wyma@gmail.com"
+                  className="text-lg underline font-medium"
+                >
+                  info.wyma@gmail.com
+                </a>
+              </div>
+              <div className="mt-1">
+                <a href="">LinkedIn</a>, <a href="">GitHub</a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
