d6b0a72806f8aca0ad6b316ad10675b041445ad0
wyma-dss <waiyanminaung@dss.com.mm>
2025-10-21 21:28:24 +0700

    feat: enhance Resume button with icon and responsive text display

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -58,7 +58,22 @@ const RootLayout = () => {
         </div>
         <div className="flex">
           <Button variant="outline" className="ms-auto">
-            Download Resume
+            <span className="md:inline-block hidden">Download</span>
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="size-4 md:hidden"
+            >
+              <path
+                d="M3 21H21M12 3V17M12 17L19 10M12 17L5 10"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+            Resume
           </Button>
         </div>
       </div>
